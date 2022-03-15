35703dee0b33f8fc7dd8e46472a2336a7a7704db
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-03-15 11:54:32 +0200

    data logged in the console through the API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 //API key :e54d193
+
+const API_URL = "http://www.omdbapi.com?apikey=e54d193";
 const App = () => {
+  const SearchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    SearchMovies("Spiderman");
+  }, []);
   return <div>yes that s true</div>;
 };
 
